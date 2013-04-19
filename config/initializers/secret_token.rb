adf114f0ebda27a662fb37f4a1e79eb8dca124f2
Michel Henrich <michelchenrich@gmail.com>
2013-04-19 20:28:01 -0300

    Startup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayne::Application.config.secret_token = '261811f75bd68f4b1f0136d953b31bbf23db5bdec237813e10093a382c847ce62362ac45a28253b84c64c948d460179dec115f14a88bb586c98c964774dd5e93'
